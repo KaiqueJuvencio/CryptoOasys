aacc33655236d4bd5ceb62272e6100b3f6c0639c
KaiqueJuvencio <kaiquejuvencio@hotmail.com>
2022-05-09 19:30:14 -0300

    :dart: Git init

---
+++ src/test/java/com/br/cryptoOasys/CryptoOasysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.cryptoOasys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoOasysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
